d1d171a6b791e2fdbc8963a976b190b35661119c
Mark Brown <o.mark.brown@gmail.com>
2019-07-27 12:51:21 -0600

    got tab working

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,45 @@
 import {
   JupyterFrontEnd, JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
-
+import {
+  ICommandPalette, MainAreaWidget
+} from '@jupyterlab/apputils';
+import{
+  Widget
+} from '@phosphor/widgets'
 /**
  * Initialization data for the Collapsible_Headings extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'Collapsible_Headings',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension Collapsible_Headings is activated!');
+  requires: [ICommandPalette],
+
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+    console.log('JupyterLab extension Collapsible_Headings is activated! v1');
+    const command: string = 'Collapsible_Headings:open'
+    app.commands.addCommand(command, {
+      label: 'Random Astronomy Picture',
+      execute: () => {
+        //console.log(widget.id, widget.title.label, widget.title.closable, widget);
+        const content = new Widget();
+        const widget = new MainAreaWidget({content});
+        widget.id = 'Collapsible_Headings';
+        widget.title.label = 'Astronomy Picture';
+        widget.title.closable = true;
+        if (!widget.isAttached) {
+          console.log('Widget was Unattached, attaching.');
+          // attach the widget to the main work area if it's not already there
+          app.shell.add(widget, 'main');
+        }
+        console.log('Activating Widget');
+        app.shell.activateById(widget.id);
+      }
+    });
+    palette.addItem({command, category:'Tutorial'});
+    console.log('Artorias!');
   }
+
 };
 
 export default extension;
