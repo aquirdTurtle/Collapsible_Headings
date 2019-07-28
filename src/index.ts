07817af06200e72d3d5b7cdf7ed921f50e8593ed
Mark Brown <o.mark.brown@gmail.com>
2019-07-27 22:59:37 -0600

    implemented basic header cell detection and logic for which cells to hide

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,10 @@ import {
   NotebookActions, NotebookPanel, INotebookModel, INotebookTracker
 } from '@jupyterlab/notebook';
 
+import {
+  Cell
+} from '@jupyterlab/cells';
+
 const plugin: JupyterFrontEndPlugin<void> = {
   activate,
   requires: [INotebookTracker],
@@ -25,47 +29,74 @@ const plugin: JupyterFrontEndPlugin<void> = {
   autoStart: true
 };
 
-function activate(
-  app: JupyterFrontEnd,
-  nbTrack: INotebookTracker ){
+function activate(app: JupyterFrontEnd,  nbTrack: INotebookTracker){
   app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
-  console.log('Colappsible_Headings Extension V10');
+  console.log('Collapsible_Headings Extension');
   nbTrack.currentChanged.connect(()=>{
-    console.log('current notebook changed');
+    //console.log('current notebook changed');
   })
   nbTrack.activeCellChanged.connect(() => {
     if (nbTrack.activeCell){
       if (nbTrack.activeCell.constructor.name === "MarkdownCell"){
-        console.log("Detected Markdown Cell!");
-        console.log(nbTrack.currentWidget);
-        for (let i = 0; i < nbTrack.currentWidget.content.widgets.length; i++) {
-          console.log('logging cell number: ', i);
+        let cell = nbTrack.activeCell;
+        let selectedHeaderInfo = getHeaderInfo(cell);
+        console.log(selectedHeaderInfo);
+        if (selectedHeaderInfo.isHeader)
+        {
+          console.log(
+            'active cell index:',
+            nbTrack.currentWidget.content.activeCellIndex
+          );
+          for (
+            let i = nbTrack.currentWidget.content.activeCellIndex+1;
+            i < nbTrack.currentWidget.content.widgets.length;
+            i++
+          ) {
+            let subCell = nbTrack.currentWidget.content.widgets[i];
+            let subCellHeaderInfo = getHeaderInfo(subCell);
+            console.log(subCellHeaderInfo);
+            if (
+              !subCellHeaderInfo.isHeader
+              || subCellHeaderInfo.headerLevel > selectedHeaderInfo.headerLevel
+            ){
+              subCell.setHidden(!subCell.isHidden);
+            } else {
+              break;
+            }
+          }
         }
       }
     }
   })
   NotebookActions.executed.connect(() => {
-    console.log('cell executed.');
+    //console.log('cell executed.');
   })
-
-  /*
-  labShell.currentChanged.connect(() => {
-    // this seems to happen near beginning of runtime but after the activate
-    // function is originally called.
-    console.log('detected labs shell current change');
-    let cw = app.shell.currentWidget;
-    console.log(cw);
-    console.log('ishidden',cw.isHidden);
-    let mwid : mWidget = {cw};
-    //mwidget = app.shell.currentWidget;
-    console.log('content',mwid.widget.content);
-    //for (let i = 0; i < app.shell.currentWidget.content.widgets.length; i++) {
-    //  console.log('logging cell number: ', i);
-    //}
-  });
-  */
 };
 
+function getHeaderInfo(cell: Cell) : {isHeader: boolean, headerLevel: number} {
+  if (cell.constructor.name !== "MarkdownCell"){
+    return {isHeader:false, headerLevel:0};
+  }
+  let text = cell.model.value.text;
+  const lines = text.split('\n');
+  const line = lines[0];
+  const line2 = lines.length > 1 ? lines[1] : undefined;
+  // logic here for determining if header and what level of header was stolen
+  // from the wonderful existing table of contents extension <3
+  let match = line.match(/^([#]{1,6}) (.*)/);
+  let match2 = line2 && line2.match(/^([=]{2,}|[-]{2,})/);
+  let match3 = line.match(/<h([1-6])>(.*)<\/h\1>/i);
+  let isHeader = (match !== null) || (match2 !== undefined) || (match3 !== null);
+  let level = 0;
+  if (match){
+    level = match[1].length;
+  } else if (match2) {
+    level = match2[1][0] === '=' ? 1 : 2;
+  } else if (match3) {
+    level = parseInt(match3[1], 10);
+  }
+  return {isHeader:isHeader, headerLevel:level};
+}
 
 export
 class ButtonExtension
@@ -89,6 +120,5 @@ implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
     });
   }
 }
-
 
 export default plugin;
